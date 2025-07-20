5245abbeccfd36f98069c161527f6093512d35f1
Gaodafeiyan <candicexfx18@gmail.com>
2025-07-20 20:10:06 +0800

    feat: switch to PostgreSQL database configuration

---
+++ src/api/subscription-plan/controllers/subscription-plan.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const { factories } = require('@strapi/strapi');
+
+module.exports = factories.createCoreController('api::subscription-plan.subscription-plan', ({ strapi }) => ({
+  
+  /** GET /api/subscription-plans/enabled */
+  async enabled(ctx) {
+    const plans = await strapi.db.query('api::subscription-plan.subscription-plan').findMany({
+      where: { enabled: { $eq: true } },
+      orderBy: { priceUSDT: 'asc' }
+    });
+    
+    return ctx.send(plans);
+  },
+
+})); 
--- src/api/subscription-plan/routes/subscription-plan.js
+++ src/api/subscription-plan/routes/subscription-plan.js
@@ -1,4 +1,12 @@
 'use strict';
-const { factories } = require('@strapi/strapi');
 
-module.exports = factories.createCoreRouter('api::subscription-plan.subscription-plan'); 
+module.exports = {
+  routes: [
+    { method: 'GET', path: '/subscription-plans', handler: 'subscription-plan.find' },
+    { method: 'GET', path: '/subscription-plans/:id', handler: 'subscription-plan.findOne' },
+    { method: 'POST', path: '/subscription-plans', handler: 'subscription-plan.create' },
+    { method: 'PUT', path: '/subscription-plans/:id', handler: 'subscription-plan.update' },
+    { method: 'DELETE', path: '/subscription-plans/:id', handler: 'subscription-plan.delete' },
+    { method: 'GET', path: '/subscription-plans/enabled', handler: 'subscription-plan.enabled' },
+  ],
+}; 
